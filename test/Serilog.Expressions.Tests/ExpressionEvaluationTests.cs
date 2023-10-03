2f35b974d7adbaece7428fcaea74976659152c9c
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2023-10-03 13:58:41 +1000

    Add support for Serilog 3.1 trace `@tr` and span `@sp` ids

--- test/Serilog.Expressions.Tests/ExpressionEvaluationTests.cs
+++ test/Serilog.Expressions.Tests/ExpressionEvaluationTests.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics;
 using System.Globalization;
 using Serilog.Events;
 using Serilog.Expressions.Runtime;
 using Serilog.Expressions.Tests.Support;
+using Serilog.Parsing;
 using Xunit;
 
 namespace Serilog.Expressions.Tests;
@@ -15,14 +17,22 @@ public class ExpressionEvaluationTests
     [MemberData(nameof(ExpressionEvaluationCases))]
     public void ExpressionsAreCorrectlyEvaluated(string expr, string result)
     {
-        var evt = Some.InformationEvent();
-
-        evt.AddPropertyIfAbsent(
-            new("User", new StructureValue(new[]
+        var evt = new LogEvent(
+            new DateTimeOffset(2025, 5, 15, 13, 12, 11, 789, TimeSpan.FromHours(10)),
+            LogEventLevel.Warning,
+            new DivideByZeroException(),
+            new MessageTemplateParser().Parse("Hello, {Name}!"),
+            new []
             {
-                new LogEventProperty("Id", new ScalarValue(42)),
-                new LogEventProperty("Name", new ScalarValue("nblumhardt")),
-            })));
+                new LogEventProperty("Name", new ScalarValue("World")),
+                new LogEventProperty("User", new StructureValue(new[]
+                {
+                    new LogEventProperty("Id", new ScalarValue(42)),
+                    new LogEventProperty("Name", new ScalarValue("nblumhardt")),
+                }))
+            },
+            ActivityTraceId.CreateFromString("1befc31e94b01d1a473f63a7905f6c9b"),
+            ActivitySpanId.CreateFromString("bb1111820570b80e"));
 
         var frFr = CultureInfo.GetCultureInfoByIetfLanguageTag("fr-FR");
         var testHelpers = new TestHelperNameResolver();
